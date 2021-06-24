135d997bb580685714f97438ad14d0025fbb2455
camila <camilailiberg@gmail.com>
2021-06-24 15:43:08 -0400

    init commit

---
+++ client/lifetrackere/src/components/App/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Home from "../Home/Home";
+import NavBar from "../NavBar/NavBar";
+
+function App() {
+	return (
+		<div className="App">
+			<NavBar />
+			<Home />
+		</div>
+	);
+}
+
+export default App;
---
+++ client/lifetrackere/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,13 @@
+import "./NavBar.css";
+
+import React from "react";
+
+function NavBar() {
+	return (
+		<div>
+			<h1>NAVBAR</h1>
+		</div>
+	);
+}
+
+export default NavBar;
